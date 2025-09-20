546dee4da4f3775d9faefd40e8c727c0444ae185
YUyu497 <165473758+YUyu497@users.noreply.github.com>
2025-09-20 22:50:15 +0800

    Add files via upload

---
+++ vehicle_showroom.lua
@@ -0,0 +1,16 @@
+-- 车辆展示系统版本信息文件
+-- Vehicle Showroom System Version Information
+
+return {
+    -- 版本号 (Version Number)
+    version = "1.0.0",
+    
+    -- 发布日期 (Release Date)
+    release_date = "2025-09-20",
+    
+    -- 下载地址 (Download URL)
+    download_url = "https://keymaster.fivem.net/",
+    
+    -- 更新日志 (Changelog)
+    changelog = "修复了版本检查功能，优化了车辆监控系统"
+}
