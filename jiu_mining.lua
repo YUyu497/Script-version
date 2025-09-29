495e6844255428e2d20c18633925d932aaad243d
YUyu497 <165473758+YUyu497@users.noreply.github.com>
2025-09-29 21:24:51 +0800

    Create jiu_mining.lua

---
+++ jiu_mining.lua
@@ -0,0 +1,16 @@
+-- 挖矿系统版本信息文件
+-- Mining System Version Information
+
+return {
+    -- 版本号 (Version Number)
+    version = "1.0.0",
+    
+    -- 发布日期 (Release Date)
+    release_date = "2025-01-27",
+    
+    -- 下载地址 (Download URL)
+    download_url = "https://keymaster.fivem.net/",
+    
+    -- 更新日志 (Changelog)
+    changelog = "- 集成ox_lib图形界面菜单\n- 添加版本检测功能\n- 改进安全系统，防止刷物品\n- 优化挖矿概率计算\n- 添加管理员命令支持"
+}
