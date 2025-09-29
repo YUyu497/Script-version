add9f18e2b93eafc283d981b443da5c26fbaa0df
YUyu497 <165473758+YUyu497@users.noreply.github.com>
2025-09-29 18:04:19 +0800

    Create Idle_idle_rewards.lua

---
+++ Idle_idle_rewards.lua
@@ -0,0 +1,16 @@
+-- AFK挂机奖励系统版本信息文件
+-- AFK Idle Rewards System Version Information
+
+return {
+    -- 版本号 (Version Number)
+    version = "1.4.0",
+    
+    -- 发布日期 (Release Date)
+    release_date = "2024-12-19",
+    
+    -- 下载地址 (Download URL)
+    download_url = "https://keymaster.fivem.net/",
+    
+    -- 更新日志 (Changelog)
+    changelog = "- 添加版本检测功能\n- 优化防刷机制\n- 改进内存管理\n- 增强错误处理\n- 支持多挂机点和多奖励类型"
+}
