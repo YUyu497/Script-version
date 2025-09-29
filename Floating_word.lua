8eafa9dc3787402dd63e89a5e7409c85e1b0eb5d
YUyu497 <165473758+YUyu497@users.noreply.github.com>
2025-09-29 18:22:32 +0800

    Create Floating_word.lua

---
+++ Floating_word.lua
@@ -0,0 +1,16 @@
+-- 漂浮字系统版本信息文件
+-- Floating Word System Version Information
+
+return {
+    -- 版本号 (Version Number)
+    version = "1.0.0",
+    
+    -- 发布日期 (Release Date)
+    release_date = "2025-01-27",
+    
+    -- 下载地址 (Download URL)
+    download_url = "https://keymaster.fivem.net/",
+    
+    -- 更新日志 (Changelog)
+    changelog = "- 初始版本发布\n- 支持创建、编辑、删除浮动文本\n- 集成ox_lib图形界面菜单\n- 支持权限管理系统\n- 添加版本检测功能"
+}
