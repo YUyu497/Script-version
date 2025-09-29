28f8eef203e3b994256d35e2776c60edd9e38b81
YUyu497 <165473758+YUyu497@users.noreply.github.com>
2025-09-29 21:32:41 +0800

    Create zidanbao.lua

---
+++ zidanbao.lua
@@ -0,0 +1,16 @@
+-- 子弹包版本信息文件
+-- Bullet Pack Version Information
+
+return {
+    -- 版本号 (Version Number)
+    version = "1.0.0",
+    
+    -- 发布日期 (Release Date)
+    release_date = "2025-01-27",
+    
+    -- 下载地址 (Download URL)
+    download_url = "https://keymaster.fivem.net/",
+    
+    -- 更新日志 (Changelog)
+    changelog = "- 初始版本发布\n- 添加子弹包使用功能\n- 支持ESX框架\n- 添加版本检测功能"
+}
