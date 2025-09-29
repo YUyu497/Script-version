6bf4d39d8a588f0d6929e78a79f21cec71f9d30b
YUyu497 <165473758+YUyu497@users.noreply.github.com>
2025-09-29 21:26:54 +0800

    Create Player_stats.lua

---
+++ Player_stats.lua
@@ -0,0 +1,16 @@
+-- 玩家数据统计系统版本信息文件
+-- Player Statistics System Version Information
+
+return {
+    -- 版本号 (Version Number)
+    version = "1.0.0",
+    
+    -- 发布日期 (Release Date)
+    release_date = "2025-01-20",
+    
+    -- 下载地址 (Download URL)
+    download_url = "https://keymaster.fivem.net/",
+    
+    -- 更新日志 (Changelog)
+    changelog = "- 基于ESX框架的玩家数据统计系统\n- 支持游戏时长、等级、邀请奖励系统\n- 集成ox_lib图形界面菜单\n- 添加月度排行榜重置功能\n- 支持邀请码验证防刷系统\n- 优化性能，减少数据库查询"
+}
