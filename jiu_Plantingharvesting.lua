58068b713e1951bf14280fa154baf2b3e623034b
YUyu497 <165473758+YUyu497@users.noreply.github.com>
2025-09-29 21:36:58 +0800

    Create jiu_Plantingharvesting.lua

---
+++ jiu_Plantingharvesting.lua
@@ -0,0 +1,16 @@
+-- 作物种植与采摘系统版本信息文件
+-- Planting and Harvesting System Version Information
+
+return {
+    -- 版本号 (Version Number)
+    version = "1.0.0",
+    
+    -- 发布日期 (Release Date)
+    release_date = "2025-01-20",
+    
+    -- 下载地址 (Download URL)
+    download_url = "https://keymaster.fivem.net/",
+    
+    -- 更新日志 (Changelog)
+    changelog = "- 初始版本发布\n- 支持作物种植与采摘功能\n- 集成挖矿系统\n- 支持ESX框架"
+}
